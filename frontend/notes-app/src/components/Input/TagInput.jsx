ba3235887e6a1779beb4d9ef0f8fbd91022c2bf8
Shehin Thoombil <shehinthoombil@gmail.com>
2025-05-29 15:15:40 +0530

    tag issue solved

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -28,7 +28,7 @@ const TagInput = ({ tags, setTags }) => {
         <div>
             {tags?.length > 0 && (
                 <div className='flex items-center gap-2 flex-wrap mt-2'>
-                    {tags.map((tag, index) => {
+                    {tags.map((tag, index) => (
                         <span key={index} className='flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded '>
                             # {tag}
                             <button
@@ -39,13 +39,13 @@ const TagInput = ({ tags, setTags }) => {
                                 <MdClose />
                             </button>
                         </span>
-                    })}
+                    ))}
                 </div>
             )}
 
             <div className='flex items-center gap-4 mt-3'>
                 <input type="text"
-                value={inputValue}
+                    value={inputValue}
                     className='text-sm bg-transparent border px-3 py-2 rounded outline-none'
                     placeholder='Add tags'
                     onChange={handleInputChange}
