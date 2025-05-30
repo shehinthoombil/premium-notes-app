4fac45c0caae5a1fa5f7170d3d958e292ffa0a0a
Shehin Thoombil <shehinthoombil@gmail.com>
2025-05-30 18:40:47 +0530

    when project run ,navigate to login page

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
@@ -7,13 +7,13 @@ import Register from './pages/Register/Register'
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path='/dashboard' exact element={<Home />} />
       <Route path='/login' exact element={<Login />} />
       <Route path='/register' exact element={<Register />} />
     </Routes>
   </Router>
 )
-
 
 const App = () => {
   return (
